358e5e0af6fc3d9d0c45a783176a29e13fb8b3f8
nonti <nonty8@gmail.com>
2024-10-05 00:37:08 +0200

    added redux persist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter} from 'react-router-dom';
-import store from './redux/store';
+import {store }from './redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </PersistGate>
     </Provider>
 );
